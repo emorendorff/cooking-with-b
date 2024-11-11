229d9a482b2f3b57283893a8ac6c3fcf55923eb4
Em Orendorff <em.orendorff@justvanilla.com>
2024-11-11 17:30:39 -0500

    Core app changes and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage.tsx";
+const Recipe1 = () => <div>Recipe 1</div>;
+const GroceryList = () => <div>Grocery List</div>;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="cooking-with-b logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>cooking-with-b</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes" element={<Recipe1 />} />
+        <Route path="/grocery-list" element={<GroceryList />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
